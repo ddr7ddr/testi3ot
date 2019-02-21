df4afec45071393bae69ea210b5de6fc98bd57a5
ddr7ddr <ddr7ddr@163.com>
2019-02-22 01:07:43 +0800

    llllite

--- template.py
+++ template.py
@@ -1,13 +1,35 @@
 import json
 import os
 
+def create_device_unique_id(device_address):
+    # 创建一个device_unique_id
+    # 然后存到Flash里
+    # device_unique_id | device_address
+    # 0001             | 201888888888
+    # return device_unique_id
+    pass
+
+def create_unique_id_to_address(from_address):
+    # 创建一个unique_id
+    # 创建一个lite modbus address
+    # 然后存到Flash里
+    # unique_id | fc | from_address | device_id | to_address
+    # 0001      | 01 | 40001:1      | 1         | 410001:1
+    # return device_unique_id
+    pass
+
+
+
+
 def read_read():
     print('rr')
 
 def post_device(device_type, device_address):
     if device_type == '1':
         print('Device Type:', device_type, ', Create a entry at table/flash for DLT645 device at device_address, ', device_address)
+        device_unique_id = create_device_unique_id(device_type, device_address)
     elif device_type == '2':
+        device_unique_id = create_device_unique_id(device_type, device_address)
         print('Device Type:', device_type, ', Create a entry at table/flash for ModbusTCP device at device_address, ', device_address)
 
 
@@ -76,29 +98,29 @@ for file_name in iterate_file_name:
 
     print('------------------read {0}-----------------{1}'.format(full_path, json_data))
     json_function_code = json_data['fc']
-    if json_data['fc'] == 'pd':
+    if json_function_code == 'pd':
         json_device_type = json_data['dt']
         json_device_address = json_data['dadd']
         function_code[json_function_code](json_device_type, json_device_address)
-    elif json_data['fc'] == 'md':
+    elif json_function_code == 'md':
         json_device_type = json_data['dt']
         json_device_unique_id = json_data['did']
         json_device_address = json_data['dadd']
         function_code[json_function_code](json_device_type, json_device_unique_id, json_device_address)
-    elif json_data['fc'] == 'dd':
+    elif json_function_code == 'dd':
         json_device_type = json_data['dt']
         json_device_unique_id = json_data['did']
         function_code[json_function_code](json_device_type, json_device_unique_id)
-    elif json_data['fc'] == 'pa':
+    elif json_function_code == 'pa':
         json_device_type = json_data['dt']
         json_from_address = json_data['fa']
         function_code[json_function_code](json_device_type, json_from_address)
-    elif json_data['fc'] == 'ma':
+    elif json_function_code == 'ma':
         json_device_type = json_data['dt']
         json_unique_id = json_data['id']
         json_from_address = json_data['fa']
         function_code[json_function_code](json_device_type, json_unique_id, json_from_address)
-    elif json_data['fc'] == 'da':
+    elif json_function_code == 'da':
         json_device_type = json_data['dt']
         json_unique_id = json_data['id']
         function_code[json_function_code](json_device_type, json_unique_id)
